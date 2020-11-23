7cbcafbe9d52e3e7df529e718aeacc36652b1383
ronaldperaza <ronaldperaza460@gmail.com>
2020-11-23 20:12:00 -0300

    firts commit

---
+++ src/employee/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    id = models.CharField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    contact = models.CharField(max_length=15)
+
+    class Meta:
+        db_table = "employee"
